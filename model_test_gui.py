7f38cf6703056d7d68bd8abf201690af0a4b7c02
Ming You <a9650615@gmail.com>
2020-10-22 15:13:53 +0800

    Remove useless code

--- model_test_gui.py
+++ model_test_gui.py
@@ -15,40 +15,8 @@ first_run = True
 cap_send = cv2.VideoCapture('v4l2src device=/dev/video0 ! video/x-raw,width=640,height=480,framerate=30/1 ! videoconvert ! appsink', cv2.CAP_GSTREAMER)
 
 process = Popen([your_exe_file_address], stdout=PIPE, stdin=PIPE, stderr=PIPE, shell=True) #, shell=True
-# # stdout, stderr = process.communicate()
-
-# # output = process.communicate(b'five.bin')[0].decode().strip()
-# # print('out', output)
-# process.poll()
-# # print('out', out.strip())4
-# # process.stdin.close()
-# # process.stdin.close()
-# # data,ww = process.communicate(b"five.bin")
-# # print(data)
-# # print(ww)
-# output = process.stdout.readline()
-# print('out', output.strip())
-# while True:
-	# output = process.stdout.readline()
-	# if output == '' and process.poll() is not None:
-	# 	break
-	# if output:
-	# 	if output.strip().find(b"Result") != -1:
-	# 		getRes = True
-
-	# 	print('out2', output.strip())
-	# if getRes == True:
-	# 	# process.communicate(b"five.bin\n")
-	# 	process.stdin.write(b'five.bin\r\n')
-	# 	process.stdin.flush()
-	# # process.stdin.close()
-	
-	# rc = process.poll()
-# 	time.sleep(0.02)
 
 def saveImgToBin(frame):
-    # image = image.resize((28,28), Image.ANTIALIAS)
-    # print(image.shape)
     image = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     image = cv2.resize(image, ( 28, 28), fx=0.2, fy=0.2, interpolation = cv2.INTER_AREA)
     
@@ -63,8 +31,6 @@ class Thread(QThread):
 
     def run(self):
         if cap_send.isOpened():
-            # ret, frame = cap_send.read()
-            # cv2.imwrite('output.png', frame)
             while True:
                 ret, frame = cap_send.read()
 
@@ -92,12 +58,11 @@ class Thread(QThread):
                     if output.strip().find(b"Result") != -1:
                         self.getRes = True
 
-                    # print('out2', output.strip())
                     self.str_signal.emit(output.strip().decode("utf-8") )
                 if self.getRes == True:
                     process.stdin.write(b'./input.bin\r\n')
                     process.stdin.flush()
-                # process.stdin.close()
+    
                 rc = process.poll()
                 #end tf
                 rgbImage = cv2.cvtColor(crop_img, cv2.COLOR_BGR2RGB)
@@ -112,8 +77,6 @@ class DisplayImageWidget(QtWidgets.QWidget):
     def __init__(self, parent=None):
         super(DisplayImageWidget, self).__init__(parent)
 
-        self.button = QtWidgets.QPushButton('Show picture')
-        # self.button.clicked.connect(self.show_image)
         self.image_frame = QtWidgets.QLabel()
         self.result_label = QtWidgets.QLabel()
         self.response_label = QtWidgets.QLabel()
@@ -129,7 +92,6 @@ class DisplayImageWidget(QtWidgets.QWidget):
         self.layout = QtWidgets.QVBoxLayout()
         self.layout.addWidget(self.result_label)
         self.layout.addWidget(self.response_label)
-        # self.layout.addWidget(self.button)
         self.layout.addWidget(self.image_frame)
         self.layout.addWidget(self.label)
         self.setLayout(self.layout)
@@ -150,15 +112,6 @@ class DisplayImageWidget(QtWidgets.QWidget):
             self.response_label.setText(split_text[1])
         else:
             self.result_label.setText(text)
-        
-        
-
-
-    @QtCore.pyqtSlot()
-    def show_image(self):
-        self.image = cv2.imread('image.jpg')
-        self.image = QtGui.QImage(self.image.data, self.image.shape[1], self.image.shape[0], QtGui.QImage.Format_RGB888).rgbSwapped()
-        self.image_frame.setPixmap(QtGui.QPixmap.fromImage(self.image))
 
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
