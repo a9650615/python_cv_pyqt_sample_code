6a0752fc417d26bc560d87331df277b15ea070bc
Ming You <a9650615@gmail.com>
2020-11-03 18:37:39 +0800

    Make detection mutli thread

--- tflite_ssd.py
+++ tflite_ssd.py
@@ -7,6 +7,8 @@ from PyQt5.QtCore import QThread, pyqtSignal, Qt
 import tflite_runtime.interpreter as tflite
 import cv2
 import numpy as np
+import threading
+import time
 
 with open("labelmap.txt", 'r') as f:
     labels = [line.strip() for line in f.readlines()]
@@ -28,52 +30,68 @@ print('w x h', width, height)
 input_mean = 127.5
 input_std = 127.5
 
+boxes = []
+classes = []
+scores = []
+image = None
+
 cap_send = cv2.VideoCapture('v4l2src device=/dev/video0 ! video/x-raw,width=640,height=480,framerate=30/1 ! videoconvert ! appsink', cv2.CAP_GSTREAMER)
+
+def tf():
+    global boxes, classes, scores, image
+
+    counter = 1
+
+    while True:
+        counter = counter + 1
+        counter = counter % 10
+
+        if image is not None and counter == 0:
+            image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+            image_resized = cv2.resize(image_rgb, (width, height))
+            input_data = np.expand_dims(image_resized, axis=0)
+            input_data = (np.float32(input_data) - input_mean) / input_std #float32
+
+            interpreter.set_tensor(input_details[0]['index'],input_data)
+            interpreter.invoke()
+
+            boxes = interpreter.get_tensor(output_details[0]['index'])[0] # Bounding box coordinates of detected objects
+            classes = interpreter.get_tensor(output_details[1]['index'])[0] # Class index of detected objects
+            scores = interpreter.get_tensor(output_details[2]['index'])[0] # Confidence of detected objects
+            # time.sleep(0.03)
 
 class Thread(QThread):
     changePixmap = pyqtSignal(QtGui.QImage)
 
     def run(self):
+        global boxes, classes, scores, image
         if cap_send.isOpened():
             # ret, frame = cap_send.read()
             # cv2.imwrite('output.png', frame)
             while True:
                 ret, frame = cap_send.read()
-                frame = cv2.flip(frame, 1)
-                
-                image = frame
-                image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+                image = frame.copy()
+                # if self.counter == 0:
                 imH, imW, _ = image.shape
-                image_resized = cv2.resize(image_rgb, (width, height))
-                input_data = np.expand_dims(image_resized, axis=0)
-                input_data = (np.float32(input_data) - input_mean) / input_std #float32
-                
-                interpreter.set_tensor(input_details[0]['index'],input_data)
-                interpreter.invoke()
-
-                boxes = interpreter.get_tensor(output_details[0]['index'])[0] # Bounding box coordinates of detected objects
-                classes = interpreter.get_tensor(output_details[1]['index'])[0] # Class index of detected objects
-                scores = interpreter.get_tensor(output_details[2]['index'])[0] # Confidence of detected objects
 
                 for i in range(len(scores)):
-                  if ((scores[i] > 0.2) and (scores[i] <= 1.0)):
+                    if ((scores[i] > 0.2) and (scores[i] <= 1.0)):
+                        # Get bounding box coordinates and draw box
+                        # Interpreter can return coordinates that are outside of image dimensions, need to force them to be within image using max() and min()
+                        ymin = int(max(1,(boxes[i][0] * imH)))
+                        xmin = int(max(1,(boxes[i][1] * imW)))
+                        ymax = int(min(imH,(boxes[i][2] * imH)))
+                        xmax = int(min(imW,(boxes[i][3] * imW)))
+                        
+                        cv2.rectangle(image, (xmin,ymin), (xmax,ymax), (10, 255, 0), 2)
 
-                    # Get bounding box coordinates and draw box
-                    # Interpreter can return coordinates that are outside of image dimensions, need to force them to be within image using max() and min()
-                    ymin = int(max(1,(boxes[i][0] * imH)))
-                    xmin = int(max(1,(boxes[i][1] * imW)))
-                    ymax = int(min(imH,(boxes[i][2] * imH)))
-                    xmax = int(min(imW,(boxes[i][3] * imW)))
-                    
-                    cv2.rectangle(image, (xmin,ymin), (xmax,ymax), (10, 255, 0), 2)
-
-                    # Draw label
-                    object_name = labels[int(classes[i])] # Look up object name from "labels" array using class index
-                    label = '%s: %d%%' % (object_name, int(scores[i]*100)) # Example: 'person: 72%'
-                    labelSize, baseLine = cv2.getTextSize(label, cv2.FONT_HERSHEY_SIMPLEX, 0.7, 2) # Get font size
-                    label_ymin = max(ymin, labelSize[1] + 10) # Make sure not to draw label too close to top of window
-                    cv2.rectangle(image, (xmin, label_ymin-labelSize[1]-10), (xmin+labelSize[0], label_ymin+baseLine-10), (255, 255, 255), cv2.FILLED) # Draw white box to put label text in
-                    cv2.putText(image, label, (xmin, label_ymin-7), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 0), 2) # Draw label text
+                        # Draw label
+                        object_name = labels[int(classes[i])] # Look up object name from "labels" array using class index
+                        label = '%s: %d%%' % (object_name, int(scores[i]*100)) # Example: 'person: 72%'
+                        labelSize, baseLine = cv2.getTextSize(label, cv2.FONT_HERSHEY_SIMPLEX, 0.7, 2) # Get font size
+                        label_ymin = max(ymin, labelSize[1] + 10) # Make sure not to draw label too close to top of window
+                        cv2.rectangle(image, (xmin, label_ymin-labelSize[1]-10), (xmin+labelSize[0], label_ymin+baseLine-10), (255, 255, 255), cv2.FILLED) # Draw white box to put label text in
+                        cv2.putText(image, label, (xmin, label_ymin-7), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 0), 2) # Draw label text
 
                 rgbImage = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 				# rgbImage = image.copy()
@@ -100,6 +118,8 @@ class DisplayImageWidget(QtWidgets.QWidget):
         th = Thread(self)
         th.changePixmap.connect(self.setImage)
         th.start()
+        t = threading.Thread(target = tf)
+        t.start()
 
     @QtCore.pyqtSlot(QtGui.QImage)
     def setImage(self, image):
